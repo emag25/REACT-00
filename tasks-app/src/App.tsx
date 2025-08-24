2b3f57648c669669236cf5ef5261e7567a92b312
emag25 <emelyapraez25@gmail.com>
2025-08-24 14:32:21 -0500

    Configuracion inicial del proyecto

---
+++ tasks-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-dvh p-6">
+        <h1 className="text-2xl font-bold text-indigo-600">Tailwind OK</h1>
+        <p className="mt-2 text-slate-600">Si ves esto estilado, Tailwind funciona.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
